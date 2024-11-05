41de1ad364aa6c405904eb95eb7ed451a8e84fe8
Ashour Abdal <33664687+ash-js@users.noreply.github.com>
2024-11-05 17:05:20 +1100

    Create Category

--- BookStore/Models/Category.cs
+++ BookStore/Models/Category.cs
@@ -1,12 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookStore.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
